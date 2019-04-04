dea2a0df65074613fd45fd3e0ea5cb6346f2186f
Christopher Wavrin <wavrin@gmail.com>
2019-04-04 15:31:03 -0500

    added vuetify to gridsome

---
+++ src/layouts/Default.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <v-toolbar class="header">
+      <v-toolbar-title>{{ $static.metaData.siteName }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn class="nav__link" to="/">Home</v-btn>
+        <v-btn class="nav__link" to="/about">About</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-container>
+      <slot/>
+    </v-container>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metaData {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+// This is the main.js file. Import global CSS and scripts here.
+// The Client API can be used here. Learn more: gridsome.org/docs/client-api
+import Vuetify from "vuetify";
+import DefaultLayout from "~/layouts/Default.vue";
+import "vuetify/dist/vuetify.min.css";
+
+export default function(Vue, { router, head, isClient }) {
+  head.link.push({
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/icon?family=Material+Icons"
+  });
+
+  Vue.use(Vuetify);
+  // Set default layout as a global component
+  Vue.component("Layout", DefaultLayout);
+}
