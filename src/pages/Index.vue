ecad5812151b501d3044dd75ffd043f4f2d7c2a0
Christopher Wavrin <wavrin@gmail.com>
2019-05-15 14:59:12 -0500

    fixes image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,9 +1,14 @@
 <template>
   <Layout>
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <div class="title">
-      <g-image align-center alt="Maureen's Readers" src="~/maureen.png" width="500"/>
-    </div>
+    <v-container fluid>
+      <v-layout justify-space-around>
+        <v-flex>
+          <div class="title">
+            <g-image alt="Maureen's Readers" src="~/maureen.png" fit="contain"/>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
     <br>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores.</p>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores.</p>
