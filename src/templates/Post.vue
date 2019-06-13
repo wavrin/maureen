cd6467c60ba36c2453585847b11b674e2a70fd2f
Christopher Wavrin <wavrin@gmail.com>
2019-06-13 16:13:13 -0500

    fixes iphone notch issue on blog posts

--- src/templates/Post.vue
+++ src/templates/Post.vue
@@ -1,23 +1,25 @@
 <template>
   <Layout>
-    <g-link class="logo" to="/updates">
-      <span class="logo__text">&larr; Updates</span>
-    </g-link>
-    <div class="post-title">
-      <h1 class="post-title__text">{{ $page.post.title }}</h1>
+    <div class="content">
+      <g-link class="logo" to="/updates">
+        <span class="logo__text">&larr; Updates</span>
+      </g-link>
+      <div class="post-title">
+        <h1 class="post-title__text">{{ $page.post.title }}</h1>
 
-      <PostMeta :post="$page.post"/>
-    </div>
-
-    <div class="post content-box">
-      <div class="post__content" v-html="$page.post.content"/>
-
-      <div class="post__footer">
-        <PostTags :post="$page.post"/>
+        <PostMeta :post="$page.post"/>
       </div>
-      <br>
-      <div class="comments">
-        <vue-disqus shortname="maureensreaders" :identifier="$page.post.title"></vue-disqus>
+
+      <div class="post content-box">
+        <div class="post__content" v-html="$page.post.content"/>
+
+        <div class="post__footer">
+          <PostTags :post="$page.post"/>
+        </div>
+        <br>
+        <div class="comments">
+          <vue-disqus shortname="maureensreaders" :identifier="$page.post.title"></vue-disqus>
+        </div>
       </div>
     </div>
   </Layout>
@@ -56,5 +58,11 @@ query Post ($path: String!) {
   text-decoration: none;
   color: var(--body-color) !important;
   font-size: 0.9em;
+}
+
+.content {
+  padding: 12px;
+  padding-left: env(safe-area-inset-left);
+  padding-right: env(safe-area-inset-right);
 }
 </style>
